2111771067590a93028ce3dda612bd22f87c2494
unknown <mayadamostafa783@gmail.com>
2018-03-11 21:22:40 +0200

    adding pages

---
+++ js/main.js
@@ -0,0 +1,104 @@
+
+$(function(){
+'use strict';
+$(window).scroll(function(){
+
+	var navbar = $('.navbar');
+  if($(window).scrollTop() >= navbar.height()){
+  	navbar.addClass('scrolled');
+
+  }
+  else {
+  	navbar.removeClass('scrolled');
+  }
+
+});
+
+});
+
+$(function() {  
+    $("html").niceScroll();
+
+});
+
+
+
+//button
+$('.from-left').hover(function (){
+   
+   $(this).find('span').eq(0).animate({
+
+   width:'100%'
+    
+   }, 200);
+
+
+}, function(){
+
+ $(this).find('span').eq(0).animate({
+
+   width:0
+    
+   }, 200);
+
+})
+
+//show first info
+$('.info').first().show().animate({width:'40%'});
+// show info on click
+$('.item').click(function(){
+ $(this)
+ .next().show().animate({width:'40%'})
+ .siblings('.info').animate({width:0},function(){ $(this).hide()});
+});
+
+$(document).ready(function(){
+
+ $('.heading').funnyText({
+ 	speed: 400,
+ 	borderColor: 'white',
+ 	activeColor: '#ffc728',
+ 	color: '#ffc728',
+ 	fontSize: '30px',
+    direction: 'both'
+ });
+  
+});
+//gallery
+$('.thumbnails img').on('click', function(){
+
+	$(this).addClass('active').siblings().removeClass()
+	$('.master-img img').hide().attr('src', $(this).attr('src')).fadeIn(500);
+});
+
+//right
+$('.master-img .fa-chevron-right').on('click',function(){
+   
+   if ($('.thumbnails .active').is(':last-child')) {
+      
+      $('.thumbnails img').eq(0).click();
+      
+   }
+   else{
+   	$('.thumbnails .active').next().click();
+   }
+
+
+
+	
+});
+//left
+
+$('.master-img .fa-chevron-left').on('click',function(){
+
+
+	  if ($('.thumbnails .active').is(':first-child')) {
+      
+      $('.thumbnails img:last').eq().click();
+      
+   }
+   else{
+   	$('.thumbnails .active').next().click();
+   }
+	
+});
